65faf950565eac61d39364a1af14e2cf37946590
jeesssik <jeesssik@gmail.com>
2023-06-18 23:31:49 -0300

    navegación con estilos

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,10 +1,14 @@
 import { Outlet, Link } from "react-router-dom";
+import '../index.css';
 
 const Layout = () => {
   // el tag link es como el tag a, pero para react-router-dom, el to es como el href
   // el tag outlet es como el tag div, pero para react-router-dom
+
+  //Armo una barra de navegación con estilos de CSS
   return (
     <>
+      
       <nav>
         <ul>
           <li>
